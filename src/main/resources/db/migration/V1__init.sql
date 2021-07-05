2dde2c2b919f6e0ba8a23d00815643d9309076a8
Daniil <ha.li.fa@yandex.ru>
2021-07-05 23:20:58 +0300

    db script change

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -7,6 +7,31 @@ create table users (
     updated_at              timestamp default current_timestamp
 );
 
+create table categories (
+    id                              bigserial primary key,
+    title                           varchar(255),
+    created_at                      timestamp default current_timestamp,
+    updated_at                      timestamp default current_timestamp
+);
+
+create table products (
+    id                              bigserial primary key,
+    title                           varchar(255),
+    price                           numeric (8, 2),
+    category_id                     bigint references categories (id),
+    created_at                      timestamp default current_timestamp,
+    updated_at                      timestamp default current_timestamp
+);
+
+create table comments (
+    id                      bigserial primary key,
+    user_id                 bigint not null references users (id),
+    product_id              bigint not null references products (id),
+    comment                 varchar(255) not null,
+    created_at              timestamp default current_timestamp,
+    updated_at              timestamp default current_timestamp
+);
+
 create table roles (
     id                      bigserial primary key,
     name                    varchar(50) not null unique,
@@ -19,58 +44,6 @@ CREATE TABLE users_roles (
           role_id                 bigint not null references roles (id),
           primary key (user_id, role_id)
 );
-
-insert into roles (name)
-values
-('ROLE_USER'),
-('ROLE_ADMIN');
-
-insert into users (username, password, email)
-values
-('user', '$2y$12$4g1SOm4vGFSF/CbT84nOzOyygKSuTtRshecj7HYOCC1xUPjhkVPWG', 'bob_johnson@gmail.com'),
-('admin', '$2y$12$4g1SOm4vGFSF/CbT84nOzOyygKSuTtRshecj7HYOCC1xUPjhkVPWG', 'john_johnson@gmail.com');
-
-insert into users_roles (user_id, role_id)
-values
-(1, 1),
-(2, 2);
-
-create table categories (
-    id                              bigserial primary key,
-    title                           varchar(255),
-    created_at                      timestamp default current_timestamp,
-    updated_at                      timestamp default current_timestamp
-);
-
-insert into categories (title)
-values
-('Food');
-
-create table products (
-    id                              bigserial primary key,
-    title                           varchar(255),
-    price                           numeric (8, 2),
-    category_id                     bigint references categories (id),
-    created_at                      timestamp default current_timestamp,
-    updated_at                      timestamp default current_timestamp
-);
-
-insert into products (title, price, category_id)
-values
-('Bread', 25.50, 1),
-('Milk', 80.40, 1),
-('Cheese', 325.00, 1),
-('Cheese2', 325.00, 1),
-('Cheese3', 325.00, 1),
-('Cheese4', 325.00, 1),
-('Cheese5', 325.00, 1),
-('Cheese6', 325.00, 1),
-('Cheese7', 325.00, 1),
-('Cheese8', 325.00, 1),
-('Cheese9', 325.00, 1),
-('Cheese10', 325.00, 1),
-('Cheese11', 325.00, 1),
-('Cheese12', 325.00, 1);
 
 create table orders (
     id                              bigserial primary key,
@@ -90,3 +63,46 @@ create table order_items (
     created_at                      timestamp default current_timestamp,
     updated_at                      timestamp default current_timestamp
 );
+
+insert into roles (name)
+values
+('ROLE_USER'),
+('ROLE_ADMIN');
+
+insert into users (username, password, email)
+values
+('user', '$2y$12$4g1SOm4vGFSF/CbT84nOzOyygKSuTtRshecj7HYOCC1xUPjhkVPWG', 'bob_johnson@gmail.com'),
+('admin', '$2y$12$4g1SOm4vGFSF/CbT84nOzOyygKSuTtRshecj7HYOCC1xUPjhkVPWG', 'john_johnson@gmail.com');
+
+insert into categories (title)
+values
+('Food');
+
+insert into products (title, price, category_id)
+values
+('Bread', 25.50, 1),
+('Milk', 80.40, 1),
+('Cheese', 325.00, 1),
+('Cheese2', 325.00, 1),
+('Cheese3', 325.00, 1),
+('Cheese4', 325.00, 1),
+('Cheese5', 325.00, 1),
+('Cheese6', 325.00, 1),
+('Cheese7', 325.00, 1),
+('Cheese8', 325.00, 1),
+('Cheese9', 325.00, 1),
+('Cheese10', 325.00, 1),
+('Cheese11', 325.00, 1),
+('Cheese12', 325.00, 1);
+
+insert into comments (user_id, product_id, comment)
+values
+(1, 1, 'consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.'),
+(1, 1, 'consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.'),
+(1, 2, 'consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.'),
+(1, 3, 'consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.');
+
+insert into users_roles (user_id, role_id)
+values
+(1, 1),
+(2, 2);
